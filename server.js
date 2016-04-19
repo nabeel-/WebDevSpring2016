8f74abd01beabb7e33d34527d2f1b17afc60d43f
nabeel el-husayni <nabeel@ccs.neu.edu>
2016-04-18 22:02:43 -0400

    dont form own connection string for mongo

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ var bodyParser = require('body-parser'),
 var connectionString = 'mongodb://127.0.0.1:27017/webdevspring2016';
 
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_URL;
 }
 
 var db = mongoose.connect(connectionString);   
