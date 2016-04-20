d73fdac578f922b926325033bdad16af9e2e3de5
nabeel el-husayni <nabeel@ccs.neu.edu>
2016-04-20 01:46:06 -0400

    clean up web services

--- public/project/app/scripts/server/services/user.service.js
+++ public/project/app/scripts/server/services/user.service.js
@@ -1,66 +1,66 @@
 module.exports = function(app, userModel) {
   var passport      = require('passport');
   var auth = authorized;
+  var LocalStrategy = require('passport-local').Strategy;
+  passport.use('local', new LocalStrategy(localStrategy));
+  passport.serializeUser(serializeUser);
+  passport.deserializeUser(deserializeUser);
+
   app.post  ('/api/project/login', passport.authenticate('local'), login);
   app.post  ('/api/project/logout',         logout);
   app.post  ('/api/project/user',           register);
   app.get   ('/api/project/loggedin',       loggedin);
+  app.get("/api/project/user/:id",          findUserById);
   app.put   ('/api/project/user/:id', auth, updateUserById);
   app.delete('/api/project/user/:id', auth, deleteUserById);
-
-  app.get("/api/project/user/:id", findUserById);
-      
+  
   function authorized (req, res, next) {
-      if (!req.isAuthenticated()) {
-          res.send(401);
-      } else {
-          next();
-      }
+    if (!req.isAuthenticated()) {
+      res.send(401);
+    } else {
+      next();
+    }
   };
-  var LocalStrategy = require('passport-local').Strategy;
-  passport.use('local', new LocalStrategy(localStrategy));
+  
   function localStrategy(username, password, done) {
-      userModel
-          .findUserByCredentials({username: username, password: password})
-          .then(
-              function(user) {
-                  if (!user) { return done(null, false); }
-                  return done(null, user);
-              },
-              function(err) {
-                  if (err) { return done(err); }
-              }
-          );
+    userModel
+    .findUserByCredentials({username: username, password: password})
+    .then(
+      function(user) {
+        if (!user) { return done(null, false); }
+        return done(null, user);
+      },
+      function(err) {
+        if (err) { return done(err); }
+      }
+      );
   }
 
-  passport.serializeUser(serializeUser);
-  passport.deserializeUser(deserializeUser);
-
   function serializeUser(user, done) {
-      done(null, user);
+    done(null, user);
   }
 
   function deserializeUser(user, done) {
-      userModel
-          .findUserById(user._id)
-          .then(
-              function(user){
-                  done(null, user);
-              },
-              function(err){
-                  done(err, null);
-              }
-          );
+    userModel
+    .findUserById(user._id)
+    .then(
+      function(user){
+        done(null, user);
+      },
+      function(err){
+        done(err, null);
+      }
+      );
   }
 
   function login(req, res) {
-      var user = req.user;
-      res.json(user);
+    var user = req.user;
+    res.json(user);
   }
 
   function logout(req, res) {
-      req.logOut();
-      res.send(200);
+    req.logOut();
+    res.send(200);
   }
 
   function loggedin(req, res) {
@@ -95,7 +95,7 @@ module.exports = function(app, userModel) {
 
   function findUserByUsername(req, res) {
     var username = req.query.username,
-        resp     = userModel.findUserByUsername(username);
+    resp     = userModel.findUserByUsername(username);
 
     resp.then(function(user) {
       res.send(user);
