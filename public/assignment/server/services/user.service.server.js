ae08b8eb137cdfe61bb16fda310a8f4f28b0a8b2
nabeel el-husayni <nabeel@ccs.neu.edu>
2016-04-27 20:18:51 -0400

    revert assignment auth in favor of project auth

--- public/assignment/server/services/user.service.server.js
+++ public/assignment/server/services/user.service.server.js
@@ -1,89 +1,89 @@
 module.exports = function(app, userModel) {
 
-  var passport      = require('passport');
-  var LocalStrategy = require('passport-local').Strategy;
-  var auth = authorized;
-  var auth_ad = authorized_admin;
-  passport.use('assignment', new LocalStrategy(localStrategy));
-  passport.serializeUser(serializeUser);
-  passport.deserializeUser(deserializeUser);
+  // var passport      = require('passport');
+  // var LocalStrategy = require('passport-local').Strategy;
+  // var auth = authorized;
+  // var auth_ad = authorized_admin;
+  // passport.use('assignment', new LocalStrategy(localStrategy));
+  // passport.serializeUser(serializeUser);
+  // passport.deserializeUser(deserializeUser);
 
-  app.post  ('/api/assignment/login', passport.authenticate('assignment'), login);
-  app.post  ('/api/assignment/logout',         logout);
-  app.post  ('/api/assignment/user',           register);
-  app.get   ('/api/assignment/loggedin',       loggedin);
-  app.get   ('/api/assignment/user/:id',       findUserById);
-  app.put   ('/api/assignment/user/:id', auth, updateUserById);
-  app.delete('/api/assignment/user/:id', auth, deleteUserById);
+  // app.post  ('/api/assignment/login', passport.authenticate('assignment'), login);
+  // app.post  ('/api/assignment/logout',         logout);
+  // app.post  ('/api/assignment/user',           register);
+  // app.get   ('/api/assignment/loggedin',       loggedin);
+  // app.get   ('/api/assignment/user/:id',       findUserById);
+  // app.put   ('/api/assignment/user/:id', auth, updateUserById);
+  // app.delete('/api/assignment/user/:id', auth, deleteUserById);
 
-  // Admin API calls
-  app.get   ('/api/assignment/admin/user',     auth_ad, findAllUsers);
-  app.get   ('/api/assignment/admin/user/:id', auth_ad, findUserById); 
-  app.post  ('/api/assignment/admin/user',     auth_ad, register);
-  app.put   ('/api/assignment/admin/user/:id', auth_ad, updateUserById);
-  app.delete('/api/assignment/admin/user/:id', auth_ad, deleteUserById);
+  // // Admin API calls
+  // app.get   ('/api/assignment/admin/user',     auth_ad, findAllUsers);
+  // app.get   ('/api/assignment/admin/user/:id', auth_ad, findUserById); 
+  // app.post  ('/api/assignment/admin/user',     auth_ad, register);
+  // app.put   ('/api/assignment/admin/user/:id', auth_ad, updateUserById);
+  // app.delete('/api/assignment/admin/user/:id', auth_ad, deleteUserById);
 
-  function authorized (req, res, next) {
-    if (!req.isAuthenticated()) {
-      res.send(401);
-    } else {
-      next();
-    }
-  };
+  // function authorized (req, res, next) {
+  //   if (!req.isAuthenticated()) {
+  //     res.send(401);
+  //   } else {
+  //     next();
+  //   }
+  // };
 
-  function authorized_admin(req, res, next) {
-    if(req.isAuthenticated && isAdmin(req.user)) {
-      next();
-    } else {
-      res.send(403);
-    }
-  }
+  // function authorized_admin(req, res, next) {
+  //   if(req.isAuthenticated && isAdmin(req.user)) {
+  //     next();
+  //   } else {
+  //     res.send(403);
+  //   }
+  // }
 
   
-  function localStrategy(username, password, done) {
-    userModel
-    .findUserByCredentials({username: username, password: password})
-    .then(
-      function(user) {
-        if (!user) { return done(null, false); }
-        return done(null, user);
-      },
-      function(err) {
-        if (err) { return done(err); }
-      }
-      );
-  }
+  // function localStrategy(username, password, done) {
+  //   userModel
+  //   .findUserByCredentials({username: username, password: password})
+  //   .then(
+  //     function(user) {
+  //       if (!user) { return done(null, false); }
+  //       return done(null, user);
+  //     },
+  //     function(err) {
+  //       if (err) { return done(err); }
+  //     }
+  //     );
+  // }
 
-  function serializeUser(user, done) {
-    done(null, user);
-  }
+  // function serializeUser(user, done) {
+  //   done(null, user);
+  // }
 
-  function deserializeUser(user, done) {
-    userModel
-    .findUserById(user._id)
-    .then(
-      function(user){
-        done(null, user);
-      },
-      function(err){
-        done(err, null);
-      }
-      );
-  }
+  // function deserializeUser(user, done) {
+  //   userModel
+  //   .findUserById(user._id)
+  //   .then(
+  //     function(user){
+  //       done(null, user);
+  //     },
+  //     function(err){
+  //       done(err, null);
+  //     }
+  //     );
+  // }
 
-  function login(req, res) {
-    var user = req.user;
-    res.json(user);
-  }
+  // function login(req, res) {
+  //   var user = req.user;
+  //   res.json(user);
+  // }
 
-  function logout(req, res) {
-    req.logOut();
-    res.send(200);
-  }
+  // function logout(req, res) {
+  //   req.logOut();
+  //   res.send(200);
+  // }
 
-  function loggedin(req, res) {
-    res.send(req.isAuthenticated() ? req.user : '0');
-  }
+  // function loggedin(req, res) {
+  //   res.send(req.isAuthenticated() ? req.user : '0');
+  // }
 
 //////////////
 
